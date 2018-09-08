1fd740afa792c89b2f12dcf7f17f4b6adb381931
Areeb-M <areebmohammed11@gmail.com>
2018-09-08 15:27:09 -0500

    Implemented compare FE opcode

--- src/cpu/CPU.cs
+++ src/cpu/CPU.cs
@@ -32,10 +32,12 @@ namespace Emulator{
 			OpcodeFunction nop = NOP;
 			OpcodeFunction jump = JUMP;
 			OpcodeFunction restart38 = RESTART38;
+			OpcodeFunction compare = COMPARE;
 			opcodeTable = new Dictionary<int, OpcodeFunction>(){
 				{0x00, nop},
 				{0xC3, jump},
-				{0xFF, restart38}
+				{0xFF, restart38},
+				{0xFE, compare},
 			};
 		}
 		
@@ -65,15 +67,32 @@ namespace Emulator{
 		}
 		
 		private void RESTART38(){
-			// Restart38: Restarts Gameboy from memoru location 0x38
+			// Restart38: Restarts Gameboy from memory location 0x38
 			memory[--SP] = (byte)(PC & 0x0000FFFF);
 			memory[--SP] = (byte)((PC & 0xFFFF0000) >> 8);
 			PC = 0x0038 + 1;
 		}
 		
-		private void COMPARE
-	
-	
+		private void COMPARE(){
+			int a = reg[A];
+			int b;
+			switch(memory[PC]){
+				case 0xFE:
+					b = memory[++PC];
+					break;
+				default:
+					Console.WriteLine("[Error]Unimplemented compary opcode detected!");
+					return;
+			}
+			int result = a - b;
+			int f = 0x0000;
+			f += ((result == 0) ? 1 : 0); f <<= 1;         // Z Flag
+			f += 1; f <<= 2;                   // N Flag
+			f += ((a&0xF) + ((-b)&0xF))&0x10; // H Flag
+			f += ((result < 0) ? 1 : 0); f <<= 4;
+			reg[F] = (byte)f;
+			PC += 1;
+		}	
 	}
 
 }
