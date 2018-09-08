ca77f8f9bfd97846ccc05c3fd6052751d69875ac
Areeb-M <areebmohammed11@gmail.com>
2018-09-08 13:20:56 -0500

    Begin interpretation loop
    Implemented NOP and JUMP

--- src/GameBoyColor.cs
+++ src/GameBoyColor.cs
@@ -6,7 +6,7 @@ namespace Emulator{
 	
 		public GameBoyColor(string romPath){
 			CPU cpu = new CPU(romPath);		
-			
+			Console.WriteLine("\n========Beginning Emulation========\n\n");
 			while(cpu.tick()){};
 		}	
 		
--- src/cpu/CPU.cs
+++ src/cpu/CPU.cs
@@ -24,22 +24,40 @@ namespace Emulator{
 	
 		Memory memory;
 		
-		Dictionary<int, Func<CPU>> opcodeTable = new Dictionary<int, Func<CPU>>();
+		delegate void OpcodeFunction();
+		Dictionary<int, OpcodeFunction> opcodeTable;
 	
 		public CPU(string romPath){
 			memory = new Memory(romPath);
+			OpcodeFunction nop = NOP;
+			OpcodeFunction jump = JUMP;
+			opcodeTable = new Dictionary<int, OpcodeFunction>(){
+				{0x00, nop},
+				{0xC3, jump}
+			};
 		}
 		
 		public bool tick(){
 			int opcode = memory[PC];
 			if(opcodeTable.ContainsKey(opcode)){
-				//opcodeTable[opcode](this);
+				opcodeTable[opcode]();
 				return true;
 			} else {
 				Console.WriteLine("Encountered unknown opcode {0:X2} while executing.", opcode);
 				return false;
 			}
 		}
+	
+		
+		private void NOP(){
+			// NOP: Doesn't do anything
+			PC += 1; // OPCODE length
+		}
+	
+		private void JUMP(){
+			PC = memory[PC+1]<<8 + memory[PC+2];
+		}
+	
 	
 	}
 
