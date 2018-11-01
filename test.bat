05f91cfe4eaff8efb0fadead63d9acb2c3bcf17d
Areeb-M <areebmohammed11@gmail.com>
2018-11-01 17:36:14 -0500

    create an rsp file to make life better

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /D "%~dp0"
-"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" -out:build\build.exe src\*.cs src\cpu\*.cs
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" @emulator.rsp
 
 cd build
 build.exe "%~dp0\ROMS\01-special.gb"
