187e6dc347ca13e9f702f8db8caa6df6a9970a8a
Areeb Mohammed <areebmohammed11@gmail.com>
2018-11-02 13:41:59 -0400

    Update Debug.cs

--- src/Debug.cs
+++ src/Debug.cs
@@ -3,21 +3,21 @@ using System.IO;
 
 namespace Emulator
 {
-	public enum OutputTarget {Console, File};
+	public enum OutputTarget {Console = 1, File = 2};
 	public static class Debug
 	{
-		private static OutputTarget target = OutputTarget.Console;
+		private static byte target = OutputTarget.Console;
 		// By default, route Debug messages to Console
 		
 		private static StreamWriter file;
 		
 		private static void Write(string message, params object[] args)
 		{
-			if (target == OutputTarget.Console)
+			if (target & OutputTarget.Console == OutputTarget.Console)
 			{
 				Console.Write(message, args);
 			}
-			if (target == OutputTarget.File)
+			if (target & OutputTarget.File == OutputTarget.File)
 			{
 				string processed_message = String.Format(message, args);
 				file.Write(processed_message);
@@ -39,6 +39,14 @@ namespace Emulator
 				LogError("Invalid log file path provided {0}", path);
 				throw;
 			}
+			target |= OutputTarget.File;
+		}
+		
+		public static void DetachTarget(OutputTarget t)
+		{
+			LogWarning("Attempting to detach {0} as an output target", t);
+			byte mask = 255 ^ t;
+			target &= mask;
 		}
 		
 		public static void LogWarning(string message, params object[] args)
