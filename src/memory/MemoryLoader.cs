5e0552846854e28f6b95362c384199f50caf2217
Areeb Mohammed <areebmohammed11@gmail.com>
2018-11-07 10:58:10 -0600

    Removed ConstructMemoryClass constructor from MemoryLoader

--- src/memory/MemoryLoader.cs
+++ src/memory/MemoryLoader.cs
@@ -4,20 +4,15 @@ namespace Emulator
 {
 	static class MemoryLoader
 	{
-		public static Memory ConstructMemoryClass(string pathToRom)
-		{
-			Memory mem = new Memory(path);
-			return mem;
-		}
 		
 		public static Memory ReloadSaveState(Memory memory, CPU cpu, string pathToSave)
 		{
-			
+			return memory;
 		}
 		
 		public static byte[] DumpSaveState(Memory memory, CPU cpu)
 		{
-			
+			return null;
 		}
 		
 		public static void DumpSaveState(Memory memory, CPU cpu, string pathToSave)
