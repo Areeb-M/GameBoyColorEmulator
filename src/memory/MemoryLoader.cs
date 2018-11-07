a6976844b116fb1b5a16c96d77218cbe1519a6f0
Areeb Mohammed <areebmohammed11@gmail.com>
2018-11-07 10:41:57 -0600

    Create Memory Loader class,

--- src/memory/MemoryLoader.cs
+++ src/memory/MemoryLoader.cs
@@ -2,6 +2,27 @@ using System;
 
 namespace Emulator
 {
-	
-	
+	static class MemoryLoader
+	{
+		public static Memory ConstructMemoryClass(string pathToRom)
+		{
+			Memory mem = new Memory(path);
+			return mem;
+		}
+		
+		public static Memory ReloadSaveState(Memory memory, CPU cpu, string pathToSave)
+		{
+			
+		}
+		
+		public static byte[] DumpSaveState(Memory memory, CPU cpu)
+		{
+			
+		}
+		
+		public static void DumpSaveState(Memory memory, CPU cpu, string pathToSave)
+		{
+			
+		}
+	}
 }
