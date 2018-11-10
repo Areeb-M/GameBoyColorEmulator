cb8c3f5be22b7256e309aefa7bb5d6c34a8d8925
Areeb-M <areebmohammed11@gmail.com>
2018-11-09 18:22:42 -0600

    Put NOP in a class

--- src/cpu/Opcodes.cs
+++ src/cpu/Opcodes.cs
@@ -2,11 +2,12 @@ using System;
 
 namespace Emulator
 {
-	
-	public static void NOP(Memory mem, Registers reg) // 0x00
+	class Opcodes
 	{
-		// Does nothing - length 1
-		reg.PC += 1;
+		public static void NOP(Memory mem, Registers reg) // 0x00
+		{
+			// Does nothing - length 1
+			reg.PC += 1;
+		}
 	}
-	
 }
