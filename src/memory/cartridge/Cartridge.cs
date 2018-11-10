5245b10331588940b93872f300089824270da3e3
Areeb-M <areebmohammed11@gmail.com>
2018-11-09 18:22:19 -0600

    Implement special IO register page table

--- src/memory/cartridge/Cartridge.cs
+++ src/memory/cartridge/Cartridge.cs
@@ -18,6 +18,7 @@ namespace Emulator
 		protected int ramBankSelect;
 		protected int romOffset;
 		protected int ramOffset;
+		protected Registers reg;
 		
 		public const int ROM_BANK_SIZE = 0x4000;
 		public const int RAM_BANK_SIZE = 0x2000;
@@ -42,6 +43,11 @@ namespace Emulator
 			romBankSelect = 0;
 			ramOffset = 0x2000;
 			romOffset = 0;			
+		}
+		
+		public void AttachRegisters(Registers reg)
+		{
+			this.reg = reg;
 		}
 		
 		public abstract void write(int index, byte val);
@@ -142,13 +148,114 @@ namespace Emulator
 										return (byte)0;
 								}
 							case 0xF:
-								
+								switch((index & 0x00F0) >> 4)
+								{
+									case 0:
+									case 1:
+									case 2:
+									case 3:
+									case 4:
+										return ReadRegister(index&0xFF);
+									default: // this should never be reached
+										return (byte)0;
+								}
 							default: // this should never be reached
 								return (byte)0;
 						}
 					default: // this should never be reached
 						return (byte)0; 
 				}
+			}
+		}
+		
+		protected byte ReadRegister(int index)
+		{
+			switch(index)
+			{
+				case 0x00:
+					return reg.P1;
+				case 0x01:
+					return reg.SB;
+				case 0x02:
+					return reg.SC;
+				case 0x04:
+					return reg.DIV;
+				case 0x05:
+					return reg.TIMA;
+				case 0x06:
+					return reg.TMA;
+				case 0x07:
+					return reg.TAC;
+				case 0x0F:
+					return reg.IF;
+				case 0x10:
+					return reg.NR10;
+				case 0x11:
+					return reg.NR11;
+				case 0x12:
+					return reg.NR12;
+				case 0x13:
+					return reg.NR13;
+				case 0x14:
+					return reg.NR14;
+				case 0x16:
+					return reg.NR21;
+				case 0x17:
+					return reg.NR22;
+				case 0x18:
+					return reg.NR23;
+				case 0x19:
+					return reg.NR14;
+				case 0x1A:
+					return reg.NR30;
+				case 0x1B:
+					return reg.NR31;
+				case 0x1C:
+					return reg.NR32;
+				case 0x1D:
+					return reg.NR33;
+				case 0x1E:
+					return reg.NR34;
+				case 0x20:
+					return reg.NR41;
+				case 0x21:
+					return reg.NR42;
+				case 0x22:
+					return reg.NR43;
+				case 0x23:
+					return reg.NR44;
+				case 0x24:
+					return reg.NR50;
+				case 0x25:
+					return reg.NR51;
+				case 0x26:
+					return reg.NR52;
+				case 0x40:
+					return reg.LCDC;
+				case 0x41:
+					return reg.STAT;
+				case 0x42:
+					return reg.SCY;
+				case 0x43:
+					return reg.SCX;
+				case 0x44:
+					return reg.LY;
+				case 0x45:
+					return reg.LYC;
+				case 0x46:
+					return reg.DMA;
+				case 0x47:
+					return reg.BGP;
+				case 0x48:
+					return reg.OBP0;
+				case 0x49:
+					return reg.OBP1;
+				case 0x4A:
+					return reg.WY;
+				case 0x4B:
+					return reg.WX;
+				case 0xFF:
+					return reg.IE:
 			}
 		}
 	}
